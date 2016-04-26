b348a1f9c0652db44e37ee129bcc2c50993f6210
Vazcore <vazcore@live.ru>
2016-04-26 13:14:56 +0300

    Independent sliders

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
@@ -6,20 +7,43 @@ class App extends React.Component {
         super();
         this.state = {
             name: 'Alexey',
-            age: 26
+            age: 26,
+            color: {
+                red: 0,
+                green: 0,
+                blue: 0
+            }
         };
+        this.update = this.onUpdate.bind(this);
     }
 
     onUpdate(e) {
-        this.setState({name: e.target.value});
+        this.setState({color: {
+                            red: ReactDOM.findDOMNode(this.refs.red).value,
+                            green: ReactDOM.findDOMNode(this.refs.green).value,
+                            blue: ReactDOM.findDOMNode(this.refs.blue).value,
+                    }
+        });
     }
 
     render() {
         return (
             <div>
-                <input type="text" onChange={this.onUpdate.bind(this)} />
-                <h1>Hola, {this.state.name}. Age: {this.state.age}</h1>
+                <Slider ref="red" update={this.update} /> {this.state.color.red} <br />
+                <Slider ref="green" update={this.update} /> {this.state.color.green}<br />
+                <Slider ref="blue" update={this.update} /> {this.state.color.blue}<br />
             </div>
+        );
+    }
+}
+
+class Slider extends React.Component {
+    render() {
+        return (
+            <input type="range"
+                   min="0"
+                   max="255"
+                   onChange={this.props.update} />
         );
     }
 }
@@ -32,5 +56,6 @@ App.propTypes = {
 App.defaultProps = {
     age: 26
 };
+
 
 export default App;
