eeb08f13e6abe5af65b5aa86313f418e0cc98644
Vazcore <vazcore@live.ru>
2016-04-26 13:58:11 +0300

    Component Mount/Unmount

--- App.js
+++ App.js
@@ -9,9 +9,9 @@ class App extends React.Component {
             name: 'Alexey',
             age: 26,
             color: {
-                red: 0,
-                green: 0,
-                blue: 0
+                red: 128,
+                green: 128,
+                blue: 128
             }
         };
         this.update = this.onUpdate.bind(this);
@@ -19,19 +19,78 @@ class App extends React.Component {
 
     onUpdate(e) {
         this.setState({color: {
-                            red: ReactDOM.findDOMNode(this.refs.red).value,
-                            green: ReactDOM.findDOMNode(this.refs.green).value,
-                            blue: ReactDOM.findDOMNode(this.refs.blue).value,
+                            red: ReactDOM.findDOMNode(this.refs.red.refs.color_input).value,
+                            green: ReactDOM.findDOMNode(this.refs.green.refs.color_input).value,
+                            blue: ReactDOM.findDOMNode(this.refs.blue.refs.color_input).value,
                     }
         });
     }
 
+
     render() {
         return (
             <div>
+                <h3>Color Sliders</h3>
                 <Slider ref="red" update={this.update} /> {this.state.color.red} <br />
                 <Slider ref="green" update={this.update} /> {this.state.color.green}<br />
                 <Slider ref="blue" update={this.update} /> {this.state.color.blue}<br />
+                <hr />
+                <Wrapper />
+            </div>
+        );
+    }
+}
+
+class Wrapper extends React.Component {
+
+    componentDidMount() {
+        this.inner_node = document.getElementById("app_inner");
+    }
+    mount() {
+        ReactDOM.render(<Counter />, this.inner_node);
+    }
+    unmount() {
+        ReactDOM.unmountComponentAtNode(this.inner_node);
+    }
+    render() {
+        return (
+            <div>
+                <button className="btn" onClick={this.mount.bind(this)}>Mount</button>
+                <button className="btn" onClick={this.unmount.bind(this)}>UnMount</button>
+                <div id="app_inner"></div>
+            </div>
+        );
+    }
+}
+
+class Counter extends React.Component {
+    constructor() {
+        super();
+        let count = 0;
+        this.state = {count};
+    }
+    increment() {
+        this.setState({count: this.state.count + 1});
+    }
+
+    componentWillMount() {
+        console.log("Before Mount");
+    }
+
+    componentDidMount() {
+        console.log("After Mounting");
+    }
+
+    componentWillUnmount() {
+        console.log("Unmounting");
+    }
+
+    render() {
+        console.log("Rendered");
+        return (
+            <div>
+                <h3>Counter: {this.state.count}</h3>
+                <button className="btn" onClick={this.increment.bind(this)}>Click me</button>
             </div>
         );
     }
@@ -40,10 +99,14 @@ class App extends React.Component {
 class Slider extends React.Component {
     render() {
         return (
-            <input type="range"
-                   min="0"
-                   max="255"
-                   onChange={this.props.update} />
+            <div class="form-group">
+                <input ref="color_input"
+                       type="range"
+                       class="form-control"
+                       min="0"
+                       max="255"
+                       onChange={this.props.update} />
+            </div>
         );
     }
 }
