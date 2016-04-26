9d50a73ac47874812903d64b515f84f4010db02c
Vazcore <vazcore@live.ru>
2016-04-26 12:18:18 +0300

    State change

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 
-export default class App extends React.Component {
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            name: 'Alexey',
+            age: 26
+        };
+    }
+
+    onUpdate(e) {
+        this.setState({name: e.target.value});
+    }
+
     render() {
         return (
             <div>
-                Hola
+                <input type="text" onChange={this.onUpdate.bind(this)} />
+                <h1>Hola, {this.state.name}. Age: {this.state.age}</h1>
             </div>
         );
     }
 }
+
+App.propTypes = {
+    name: React.PropTypes.string,
+    age: React.PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+    age: 26
+};
+
+export default App;
